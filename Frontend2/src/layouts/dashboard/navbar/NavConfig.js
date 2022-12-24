6425faaf05a155a2d3fea029c3f6049c459cd3cc
Thành Đạt <54407533+thanhdat24@users.noreply.github.com>
2022-12-24 23:05:18 +0700

    Initialize FE

---
+++ Frontend2/src/layouts/dashboard/navbar/NavConfig.js
@@ -0,0 +1,46 @@
+// components
+import SvgIconStyle from '../../../components/SvgIconStyle';
+
+// ----------------------------------------------------------------------
+
+const getIcon = (name) => <SvgIconStyle src={`/icons/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+
+const ICONS = {
+  user: getIcon('ic_user'),
+  ecommerce: getIcon('ic_ecommerce'),
+  analytics: getIcon('ic_analytics'),
+  dashboard: getIcon('ic_dashboard'),
+};
+
+const sidebarConfig = [
+  // GENERAL
+  // ----------------------------------------------------------------------
+  {
+    subheader: 'general v3.3.0',
+    items: [
+      { title: 'Tổng quan', path: '/dashboard/analytics', icon: ICONS.analytics },
+      // { title: 'Two', path: '/dashboard/two', icon: ICONS.ecommerce },
+      // { title: 'Three', path: '/dashboard/three', icon: ICONS.analytics },
+    ],
+  },
+
+  // MANAGEMENT
+  // ----------------------------------------------------------------------
+  {
+    subheader: 'management',
+    items: [
+      {
+        title: 'user',
+        path: '/dashboard/user',
+        icon: ICONS.user,
+        children: [
+          { title: 'Four', path: '/dashboard/user/four' },
+          { title: 'Five', path: '/dashboard/user/five' },
+          { title: 'Six', path: '/dashboard/user/six' },
+        ],
+      },
+    ],
+  },
+];
+
+export default sidebarConfig;
